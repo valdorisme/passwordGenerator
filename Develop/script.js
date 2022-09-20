eb47b8121b53508fd700956b6d498a29f12b65c6
Valerie <valdorisme@yahoo.com>
2022-09-20 13:48:52 -0400

    Second commit: assigning variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// assignment of numbers and symbols
 var number = '1234567890';
-var symbol = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var symbol = " !"\'()*&+,-./:$%;<=>?@[]^_`{|}#~"
+// assignment of uppercase and lowercase letters
+var upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var lower = "qwertyuiopasdfghjklzxcvbnm"
+
+
+function generatePassword() {
+  var userInput = window.prompt('How many characters would you like your password to have? Your password should be at least 8 characters and no more than 128 characters')
+
+  var characterLength = parseFloat(userInput)
+
+}
 
 // Write password to the #password input
 function writePassword() {
