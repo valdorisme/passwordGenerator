d9efe57a9649123ec0aaa572a6315bfd287e918e
Valerie <valdorisme@yahoo.com>
2022-09-20 13:05:37 -0400

    First commit

---
+++ Develop/script.js
@@ -0,0 +1,16 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var number = '1234567890';
+var symbol = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
