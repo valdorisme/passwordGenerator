6fd0cca0d99da213d4339caa06388e38be0e5af8
Valerie <valdorisme@yahoo.com>
2022-09-20 19:08:14 -0400

    Actual third commit: rearranging assigned variables and starting if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var length = document.querySelector(input[name="length"]);
 var lower = document.querySelector(input[name="lower"]);
 var upper = document.querySelector(input[name="upper"]);
 var number = document.querySelector(input[name="number"]);
 var symbol = document.querySelector(input[name="symbol"]);
 
 // creating a function containing password requirements
-function requirements
+function generatePassword
 // assignment of numbers and symbols
   number = '1234567890';
   symbol = " !"\'()*&+,-./:$%;<=>?@[]^_`{|}#~"
 // assignment of uppercase and lowercase letters
   upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
   lower = "qwertyuiopasdfghjklzxcvbnm"
+// assigning password length
+  var length = prompt('Password must contain 8-128 characters. How many characters would you like to use?')
+  length = parseFloat(length)
 
+  if (length<8) {
+    alert("Password must be at least 8 characters long");
+    return "";
+  }
 
-function generatePassword() {
-  var userInput = window.prompt('How many characters would you like your password to have? Your password should be at least 8 characters and no more than 128 characters')
-
-  var characterLength = parseFloat(userInput)
-
-}
+  if (length>128) {
+    alert("Your password is too long. Must contain 128 characters or less.");
+    return "" 
+  }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  if (generatePassword.number not in password)
+    alert("Your password should contain a number")
+    
   passwordText.value = password;
 
 }
