e6176c83b5dde7f030f1579fa096303c61e35463
Valerie <valdorisme@yahoo.com>
2022-09-22 17:26:46 -0400

    Rearranging variables

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,17 +1,26 @@
 // Assignment Code
-// Make four different variables, each consisting of an array of characters
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-console.log(lowerCase)
-// Even make the integers a string, so they are easier to manage (0-9)
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-console.log(upperCase)
-var number = ['1', '2' '3', '4', '5', '6', '7', '8', '9', '0'];
-console.log(number)
-var symbol = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\'', ']', '^', '_', '`', '{', '|', '}', '~' ];
+var generateBtn = document.querySelector('#generate')
+
 
 // creating a function containing password requirements
 function generatePassword() {
-// assigning password length
+
+  // Make four different variables, each consisting of an array of characters
+  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  //console.log(lowerCase)
+  // Even make the integers a string, so they are easier to manage (0-9)
+  
+  var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  //console.log(upperCase)
+  
+  var number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  //console.log(number)
+  
+  var symbol = [' ', '!', '"', '#', '$', '%', '&', '(', ')', 
+  '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', 
+  '\'', ']', '^', '_', '`', '{', '|', '}', '~' ];
+
+  // assigning password length
   var length = prompt('Password must contain 8-128 characters. How many characters would you like to use?')
   length = parseFloat(length)
 
@@ -25,14 +34,42 @@ function generatePassword() {
     alert("Your password is too long. Must contain 128 characters or less.");
     return "" 
   }
+
+  //Have funtion create random code if length is within defined range
+  else (length>8 & length<128)
+    return String.random([lowerCase, upperCase, number,symbol])
+  
+
 }
+//console.log(generatePassword)
 
 // Write password to the #password input
-function writePassword() {
+
+function writePassword(values) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  // Creating if statements to ensure required characters are in the password
+  passwordText.value = password;
+
+  for (var i = 0; i < values; i++) {
+    password += generatePassword();
+  }
+  return password;
+}
+
+  
+  
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+// Creating if statements to ensure required characters are in the password
 
   // if (generatePassword.number not in password)
   //   alert("Your password should contain a number")
@@ -46,9 +83,3 @@ function writePassword() {
   // if (generatePassword.lower not in password)
   //   alert("Your password should contain a lowercase letter")
     
-  passwordText.value = password;
-
-}
-var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
